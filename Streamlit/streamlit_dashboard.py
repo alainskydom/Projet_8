af660e8ec2b696d3c4d4ec877ebae5da295c0ef8
alainskydom <alain.scialom@gmail.com>
2025-05-08 17:55:59 +0200

    Update streamlit_dashboard.py

--- Streamlit/streamlit_dashboard.py
+++ Streamlit/streamlit_dashboard.py
@@ -47,21 +47,21 @@ if st.button("Obtenir la prédiction via API"):
             st.metric(label="Probabilité de défaut", value=f"{proba*100:.2f} %")
 
 
-            st.sidebar.write("*Caractéritiques du client :**", result["features"])
+            #st.sidebar.write("*Caractéritiques du client :**", result["features"])
             
-            st.subheader("🧾 Comparaison client vs moyenne (5 variables clés)")
+            st.sidebar.subheader("🧾 Comparaison client vs moyenne (5 variables clés)")
             df_compare = pd.DataFrame({
                 "Valeur client": result["features"],
                 "Moyenne globale": result["global_means"]
             })
-            st.dataframe(df_compare)
+            st.sidebar.dataframe(df_compare)
 
-            st.subheader("📉 Visualisation comparative")
+            st.sidebar.subheader("📉 Visualisation comparative")
             fig, ax = plt.subplots(figsize=(8, 4))
             df_compare.plot(kind="bar", ax=ax)
             plt.xticks(rotation=45, ha="right")
             plt.tight_layout()
-            st.pyplot(fig)
+            st.sidebar.pyplot(fig)
 
             st.subheader("🔍 Interprétation SHAP des variables clés")
             shap_df = pd.DataFrame.from_dict(result["shap_values"], orient="index", columns=["SHAP value"])
