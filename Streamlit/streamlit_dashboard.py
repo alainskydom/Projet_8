6460f577ab4492a99a285e1a8c3b14711bcfe2e4
alainskydom <alain.scialom@gmail.com>
2025-05-11 16:17:39 +0200

    Update streamlit_dashboard.py

--- Streamlit/streamlit_dashboard.py
+++ Streamlit/streamlit_dashboard.py
@@ -82,6 +82,18 @@ if st.button("Obtenir la prédiction via API"):
     except Exception as e:
         st.error(f"Erreur lors de la connexion à l'API : {e}")
 
+
+@st.cache()
+def load_feature_importance():
+    # Requête permettant de récupérer la liste des features importance
+    data_json = requests.get(URL_API + "load_feature_importance")
+    data = data_json.json()
+      # Récupération des valeurs sans les [] de la réponse
+    lst_id = []
+    for i in data:
+        lst_id.append(i)
+    return lst_id
+
 # Afficher les graphiques des variables:
  
 st.sidebar.header("Plus d'informations")
