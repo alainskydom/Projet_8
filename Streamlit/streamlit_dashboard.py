bb13adf522db995a14f658121de6c5253189623d
alainskydom <alain.scialom@gmail.com>
2025-05-18 22:33:26 +0200

    Update streamlit_dashboard.py

--- Streamlit/streamlit_dashboard.py
+++ Streamlit/streamlit_dashboard.py
@@ -129,13 +129,13 @@ df_ = pd.read_csv(r"Streamlit/df_api_1000.csv")
 if "dropdown_selected" not in st.session_state:
     st.session_state.dropdown_selected = None
 variables_1=['CREDIT_TERM','DAYS_BIRTH', "DAYS_EMPLOYED", "AMT_ANNUITY", "CREDIT_INCOME_PERCENT","ANNUITY_INCOME_PERCENT"]
-options_1 = ["Sélectionnez une option", variables_1]
+#options_1 = ["Sélectionnez une option", variables_1]
 variables_2=['CREDIT_TERM','DAYS_BIRTH', "DAYS_EMPLOYED", "AMT_ANNUITY", "CREDIT_INCOME_PERCENT","ANNUITY_INCOME_PERCENT"]
-options_2 = ["Sélectionnez une option", variables_2]
-features_1=st.sidebar.selectbox("Sélectionnez une première caractéristique :", options_1, key="dropdown_selected", index=None)
+#options_2 = ["Sélectionnez une option", variables_2]
+features_1=st.sidebar.selectbox("Sélectionnez une première caractéristique :", variables_1, key="dropdown_selected", index=None)
 st.sidebar.write("Vous avez selectionné ", features_1)
 
-features_2=st.sidebar.selectbox("Sélectionnez une deuxième caractéristique :", options_2, key="dropdown_selected", index=None)
+features_2=st.sidebar.selectbox("Sélectionnez une deuxième caractéristique :", variables_2, key="dropdown_selected", index=None)
 st.sidebar.write("Vous avez selectionné ", features_2)
 
 # Set the style of plots
