408381b3f0e346bfcbcc629e01b2ca3262ead478
alainskydom <alain.scialom@gmail.com>
2025-05-18 21:26:51 +0200

    Update streamlit_dashboard.py

--- Streamlit/streamlit_dashboard.py
+++ Streamlit/streamlit_dashboard.py
@@ -44,6 +44,32 @@ if st.button("Obtenir la prédiction via API"):
                 st.success("✅ Prêt accordé")
 
             st.metric(label="Probabilité de défaut", value=f"{proba*100:.2f} %")
+            if st.button("🧾 Comparaison client vs moyenne (5 variables clés)"):
+            #st.sidebar.subheader("🧾 Comparaison client vs moyenne (5 variables clés)")
+                df_compare = pd.DataFrame({
+                "Valeur client": result["features"],
+                "Moyenne globale": result["global_means"]})
+                st.dataframe(df_compare)
+
+                st.sidebar.subheader("📉 Visualisation comparative")
+                fig, ax = plt.subplots(figsize=(8, 4))
+                df_compare.plot(kind="bar", ax=ax)
+                plt.xticks(rotation=45, ha="right")
+                plt.tight_layout()
+                st.sidebar.pyplot(fig)
+            else:
+                st.error("Problème technique")
+                
+            if st.button("🔍 Interprétation SHAP des variables clés"):
+                shap_df = pd.DataFrame.from_dict(result["shap_values"], orient="index", columns=["SHAP value"])
+                shap_df = shap_df.sort_values("SHAP value", key=abs, ascending=True)
+                fig2, ax2 = plt.subplots()
+                shap_df.plot(kind="barh", legend=False, ax=ax2)
+                ax2.set_title("Impact des variables sur la prédiction")
+                plt.tight_layout()
+                st.pyplot(fig2)
+            else:
+                st.error("Problème technique")
         else:
             st.warning(f"Erreur API : {response.status_code}")
             st.write(response.json())
@@ -53,30 +79,7 @@ if st.button("Obtenir la prédiction via API"):
 
             #st.sidebar.write("*Caractéritiques du client :**", result["features"])
             
-if st.button("🧾 Comparaison client vs moyenne (5 variables clés)"):
-            #st.sidebar.subheader("🧾 Comparaison client vs moyenne (5 variables clés)")
-    df_compare = pd.DataFrame({
-    "Valeur client": result["features"],
-    "Moyenne globale": result["global_means"]})
-            
-    st.dataframe(df_compare)
 
-    st.sidebar.subheader("📉 Visualisation comparative")
-    fig, ax = plt.subplots(figsize=(8, 4))
-    df_compare.plot(kind="bar", ax=ax)
-    plt.xticks(rotation=45, ha="right")
-    plt.tight_layout()
-    st.sidebar.pyplot(fig)
-
-                
-if st.button("🔍 Interprétation SHAP des variables clés"):
-    shap_df = pd.DataFrame.from_dict(result["shap_values"], orient="index", columns=["SHAP value"])
-    shap_df = shap_df.sort_values("SHAP value", key=abs, ascending=True)
-    fig2, ax2 = plt.subplots()
-    shap_df.plot(kind="barh", legend=False, ax=ax2)
-    ax2.set_title("Impact des variables sur la prédiction")
-    plt.tight_layout()
-    st.pyplot(fig2)
 
        
 
