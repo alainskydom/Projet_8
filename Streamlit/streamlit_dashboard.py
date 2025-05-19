cbe66fc3e920618c4bfbc3901c904da200727bee
alainskydom <alain.scialom@gmail.com>
2025-05-19 20:38:33 +0200

    Update streamlit_dashboard.py

--- Streamlit/streamlit_dashboard.py
+++ Streamlit/streamlit_dashboard.py
@@ -48,19 +48,19 @@ if st.button("Obtenir la prédiction via API"):
 
             #st.sidebar.write("*Caractéritiques du client :**", result["features"])
             
-            st.sidebar.subheader("🧾 Comparaison client vs moyenne (5 variables clés)")
-            df_compare = pd.DataFrame({
-                "Valeur client": result["features"],
-                "Moyenne globale": result["global_means"]
-            })
-            st.sidebar.dataframe(df_compare)
+            #st.sidebar.subheader("🧾 Comparaison client vs moyenne (5 variables clés)")
+            #df_compare = pd.DataFrame({
+                #"Valeur client": result["features"],
+                #"Moyenne globale": result["global_means"]
+            #})
+            #st.sidebar.dataframe(df_compare)
 
-            st.sidebar.subheader("📉 Visualisation comparative")
-            fig, ax = plt.subplots(figsize=(8, 4))
-            df_compare.plot(kind="bar", ax=ax)
-            plt.xticks(rotation=45, ha="right")
-            plt.tight_layout()
-            st.sidebar.pyplot(fig)
+            #st.sidebar.subheader("📉 Visualisation comparative")
+            #fig, ax = plt.subplots(figsize=(8, 4))
+            #df_compare.plot(kind="bar", ax=ax)
+            #plt.xticks(rotation=45, ha="right")
+            #plt.tight_layout()
+            #st.sidebar.pyplot(fig)
 
             st.subheader("🔍 Interprétation SHAP des variables clés")
             shap_df = pd.DataFrame.from_dict(result["shap_values"], orient="index", columns=["SHAP value"])
